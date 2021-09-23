ba683644ec677db89010ca5f13f908d868ce62c5
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-23 20:38:32 +0800

    1 file modified

--- src/Tile.js
+++ src/Tile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Tile({ name, keyTrigger, keyCode, url }) {
+  const [active, setActive] = useState(false);
   useEffect(() => {
     document.addEventListener('keydown', (e) => {
       if (e.keyCode == keyCode) playSound();
@@ -14,11 +15,18 @@ export default function Tile({ name, keyTrigger, keyCode, url }) {
 
   function playSound() {
     const audioTag = document.getElementById(keyTrigger);
+    const pad = document.getElementById(name);
     audioTag.currentTime = 0;
     audioTag.play();
+    setActive(true);
+    // setTimeout(setActive(false), 1000);
   }
   return (
-    <div className="drum-pad" id={name} onClick={playSound}>
+    <div
+      className={active ? 'drum-pad playing' : 'drum-pad'}
+      id={name}
+      onClick={playSound}
+    >
       <audio src={url} id={keyTrigger} className="clip" />
       <h4 className="name">{name}</h4>
       <p className="key-trigger">{keyTrigger}</p>
