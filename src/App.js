315ffe55135dfe9a777c52fcb1a2654fce120bdc
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-23 19:24:52 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
+import Tile from './Tile';
 import './style.css';
 
 export default function App() {
-  const [, setMarkdown] = useState([
-    {name:'snare', }
-  ]);
+  const [, setMarkdown] = useState([{ name: 'snare' }]);
 
-  return (<div>
-    <div id="drum-machine">
-      <div id="display">
-
+  return (
+    <div>
+      <div id="drum-machine">
+        <div id="display">
+          <Tile name="Snare" key="w" />
+          <Tile name="Snare" key="w" />
+          <Tile name="Snare" key="w" />
+          <Tile name="Snare" key="w" />
+        </div>
       </div>
     </div>
-  </div>);
+  );
 }
