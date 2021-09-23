e339beea4f046a3613992db6ce7baef49363951c
mart-anthony-stark <martsalazar0@gmail.com>
2021-09-23 17:39:52 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
-  const [, setMarkdown] = useState({
-
-  });
+  const [, setMarkdown] = useState([
+    {name:'snare', }
+  ]);
 
   return (<div>
     <div id="drum-machine">
       <div id="display">
-        
+
       </div>
     </div>
   </div>);
